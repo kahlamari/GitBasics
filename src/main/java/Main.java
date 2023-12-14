ad52349d7af7c415bd7531917d64104ff2e1b0b1
kahlamari <email@vincent-kahl.net>
2023-12-14 10:46:06 +0100

    Add 10xPrintln

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("Hello World");
-    }
-
-    public static test(String test) {
-        System.out.println("Test");
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Hello World");
+        }
     }
 }
