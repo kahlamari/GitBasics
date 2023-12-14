7c9c320cbd7d6c831067fad64a75ac61f99969d8
kahlamari <email@vincent-kahl.net>
2023-12-14 09:53:40 +0100

    Add Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
     }
+
+    public static test(String test) {
+        System.out.println("Test");
+    }
 }
