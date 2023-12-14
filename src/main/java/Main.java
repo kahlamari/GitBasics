e4a6108bfec761975773c65c101f1a8bfeda5e7d
kahlamari <email@vincent-kahl.net>
2023-12-14 10:55:52 +0100

    Merge with Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,9 @@ public class Main {
         for(int i = 0; i < 10; i++) {
             System.out.println("Hello World");
         }
+        for (int i = 3; i > 0; i--) {
+            System.out.println("Hello Git!" + i);
+        }
+        System.out.println("Hello World");
     }
 }
